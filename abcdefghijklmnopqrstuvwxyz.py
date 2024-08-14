5c600da3813e6be0a8ce2fb1f45738778c7a0b68
ashgold123 <asher.golden30@gmail.com>
2024-08-14 12:42:34 -0500

    Add files via upload
    asssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss why idk

---
+++ abcdefghijklmnopqrstuvwxyz.py
@@ -0,0 +1,7 @@
+#goal is for bart to write i will not take over the school with my robotic lisa\
+for poop in range(0+1,61):
+    print(poop,"i will not take over the school with my robotic lisa")
+counter=60
+while counter < 120:
+    counter=counter+1 
+    print(counter,"i will not take over the school again with my robotic lisa")
